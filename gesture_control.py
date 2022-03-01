9a759eb2aeea7782dc62d0db9b0ddeddfb8b04f3
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-01 20:02:48 +0530

    fixed error with index out of range, when putting hand all landmark may not be readed, if we put full list length not equal to zero, it may not be contain our lanmark id, so i put full list lenght > 8, it will help us in case of Index out of Range!!

--- gesture_control.py
+++ gesture_control.py
@@ -14,14 +14,20 @@ while True:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = hand.process(img_rgb)
     # print(result.multi_hand_landmarks)
+    lm_list = []
     if result.multi_hand_landmarks:
-        lm_list = []
         for hand_lm in result.multi_hand_landmarks:
             for id, lm in enumerate(hand_lm.landmark):
                 height, width, channel = img.shape
                 x, y = int(lm.x * width), int(lm.y * height)
                 lm_list.append([id, x, y])
-                print(lm_list)
+                # print(lm_list)
+                if len(lm_list) > 8:
+                    # print(lm_list[4], lm_list[8])
+
+                    x1, y1 = lm_list[4][1], lm_list[4][2]
+                    print((x1, y1))
+
             mp_draw.draw_landmarks(img, hand_lm, mp_hand.HAND_CONNECTIONS)
 
     c_time = time.time()
