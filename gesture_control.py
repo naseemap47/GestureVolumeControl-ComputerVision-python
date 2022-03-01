68815ebe708d43b90c931c737f43d69f20cce787
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-01 20:52:04 +0530

    Code to control linux system taken from github, and pasted

--- gesture_control.py
+++ gesture_control.py
@@ -10,6 +10,30 @@ mp_hand = mp.solutions.hands
 hand = mp_hand.Hands()
 mp_draw = mp.solutions.drawing_utils
 
+import subprocess
+
+def get_master_volume():
+    proc = subprocess.Popen('/usr/bin/amixer sget Master', shell=True, stdout=subprocess.PIPE)
+    amixer_stdout = str(proc.communicate()[0],'UTF-8').split('\n')[4]
+    proc.wait()
+    find_start = amixer_stdout.find('[') + 1
+    find_end = amixer_stdout.find('%]', find_start)
+    return float(amixer_stdout[find_start:find_end])
+
+
+def set_master_volume(volume):
+    val = volume
+    val = float(int(val))
+    proc = subprocess.Popen('/usr/bin/amixer sset Master ' + str(val) + '%', shell=True, stdout=subprocess.PIPE)
+    proc.wait()
+
+
+print("Current volume: ", get_master_volume())
+set_master_volume(0)
+print("Current volume (changed): ", get_master_volume())
+set_master_volume(50)
+print("Current volume (changed): ", get_master_volume())
+
 while True:
     success, img = cap.read()
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -27,13 +51,13 @@ while True:
                     # print(lm_list[4], lm_list[8])
                     x1, y1 = lm_list[4][1], lm_list[4][2]
                     x2, y2 = lm_list[8][1], lm_list[8][2]
-                    cx, cy = (x1 + x2) // 2, (y1 + y2) //2
+                    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
                     cv2.circle(img, (x1, y1), 8, (255, 0, 255), cv2.FILLED)
                     cv2.circle(img, (x2, y2), 8, (255, 0, 255), cv2.FILLED)
                     cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
                     cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
 
-                    length = math.hypot(x2- x1, y2 - y1)
+                    length = math.hypot(x2 - x1, y2 - y1)
                     # print(length)
                     if length < 26:
                         cv2.circle(img, (cx, cy), 8, (0, 255, 0), cv2.FILLED)
