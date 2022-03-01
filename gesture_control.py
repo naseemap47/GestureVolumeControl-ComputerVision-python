c6abf372ac3a15084eb09350a38181dccea1b4f9
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-01 19:15:09 +0530

    added frame_per_sec to display

--- gesture_control.py
+++ gesture_control.py
@@ -3,9 +3,20 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+p_time = 0
 
 while True:
     success, img = cap.read()
+
+
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+    cv2.putText(
+        img, f'FPS: {int(fps)}', (10, 60),
+        cv2.FONT_HERSHEY_PLAIN, 2,
+        (0, 255, 255), 2
+    )
 
     cv2.imshow("Web-cam", img)
     cv2.waitKey(1)
